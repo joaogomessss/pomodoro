d284043acef39017bc2f99d5fdd90b4cef586f71
João Gomes <joaossemog@gmail.com>
2023-03-30 10:27:32 -0300

    ss

--- javascript.js
+++ javascript.js
@@ -50,6 +50,8 @@ let actualSetting = { hours:"" , minutes:"" , seconds:""};
 
 let display;
 
+let time;
+
 
 function startTimer(time) {
 
@@ -132,6 +134,75 @@ postMessage(display);
 
 
 onmessage = (event) => {
+
+  console.log(event.data)
+
+  time = event.data;
+
+
+    actualSetting.hours = time.hours ;
+    actualSetting.minutes = time.minutes ;
+    actualSetting.seconds = time.seconds ;
+    
+    
+    if(condition != "paused" )   {   
+    
+    hours   = time.hours ;
+    minutes = time.minutes ;
+    seconds = time.seconds;
+    
+    }
+    
+    initialTime = new  Date().getTime() + 1000 + hours * 3600000 + minutes * 60000 + seconds * 1000;
+    
+    
+    timerId = setInterval(() => {
+    
+    
+    
+    interTime = new Date().getTime() ;
+    
+    console.log(timeLeft);
+    
+    
+    
+    timeLeft = Math.floor((initialTime - interTime) / 1000)  ;
+    
+    if(timeLeft == 0){ clearInterval(timerId)};
+    
+    
+    
+    /*
+    
+    hours   = Math.floor(timeLeft / 3600 )
+    minutes = Math.floor((timeLeft % 3600) / 60 );
+    seconds = Math.floor(timeLeft % 60 );
+    
+    if(hours   < 10 ){ hours   = "0" + hours   };
+    if(minutes < 10 ){ minutes = "0" + minutes };
+    if(seconds < 10 ){ seconds = "0" + seconds };
+    
+    display = hours + ":" + minutes + ":" + seconds ;
+    
+    */
+    
+    
+    
+    
+    postMessage(timeLeft);
+    
+    
+    }, 1000)
+    
+    
+    
+    }
+
+
+
+
+
+/* 
 const { type, data } = event.data;
 switch (type) {
 case 'start':
@@ -140,8 +211,8 @@ break;
 case 'stop':
 stopTimer();
 break;
-}
-};
+}*/
+
 `;
 const blob = new Blob([workerScript], { type: 'application/javascript' });
 const blobUrl = URL.createObjectURL(blob);
@@ -152,7 +223,7 @@ startButton.onclick  = () => Start();
 
 function Start(){ // this function will send a message to the webworker start the pomodor
     
-    worker.postMessage({ type: 'start', data: time }); 
+    worker.postMessage(time); 
     startButton.textContent = "Pause";
     startButton.onclick  = () => pause();
         
